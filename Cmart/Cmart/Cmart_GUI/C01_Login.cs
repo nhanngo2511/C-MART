280bdd11688a41bf7a63ec22ccd16c34c5d22cf5
phuchoang <phucmai2401@gmail.com>
2016-12-12 22:42:35 +0700

    Gui_Login
    Gui_Login

--- Cmart/Cmart/Cmart_GUI/C01_Login.cs
+++ Cmart/Cmart/Cmart_GUI/C01_Login.cs
@@ -7,23 +7,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Cmart.BUS;
+using Cmart.Cmart_GUI;
 namespace Cmart
 {
     public partial class C01_Login : Form
     {
-        C01_Login C01 = new C01_Login();
+        validation a = new validation();
+        BUS.C01_Login login;
         public C01_Login()
         {
             InitializeComponent();
         }
-
-        private void btnLogin_Click(object sender, EventArgs e)
+        private void btnLogin_Click_1(object sender, EventArgs e)
         {
-            if (!C01.RequireLogin())
+            string message = null;
+            string UserName = txtName.Text;
+            string Password = txtSupplier.Text;
+            if (!a.Required(txtName))
             {
-                MessageBox.Show("Information is require");
+                message += "Name is a required field\n";
             }
+            if (!a.Required(txtSupplier))
+            {
+                message += "Password is a required field\n";
+            }
+            else{
+                login = new BUS.C01_Login(UserName, Password);
+                var acc = login.checkAccount();
+                if (acc)
+                {
+                    this.Hide();
+                    Cmart_GUI.C08_Product product = new Cmart_GUI.C08_Product();
+                    product.ShowDialog();
+                    this.Close();
+                }
+                else
+                {
+                    message += "User or Password is wrong\n ";
+                }
+            }
+            if (message != null)
+            {
+                MessageBox.Show(message);
+            }
+
         }
     }
 }
