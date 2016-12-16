eea1ae86346a638a66ba4a0cce0bc5d82fd98377
phuchoang <phucmai2401@gmail.com>
2016-12-17 00:23:41 +0700

    Add files via upload

--- CmartForm/Cmart/BUS/C01_LoginBUS.cs
+++ CmartForm/Cmart/BUS/C01_LoginBUS.cs
@@ -9,5 +9,35 @@ namespace Cmart.BUS
 {
     class C01_LoginBUS
     {
+        string userName;
+        Account acc;
+        CMART1Entities db = new CMART1Entities();
+        public C01_LoginBUS(string Username)
+        {
+            this.userName = Username;
+        }
+
+        public Account FindAcc()
+        {
+            db = new CMART1Entities();
+            acc= db.Accounts.FirstOrDefault(x => x.Username == userName);
+            return acc;
+        }
+        public string getPosition()
+        {
+            return acc.Auth;
+        }
+        public string getFullName()
+        {
+            return acc.Name;
+        }
+        public string getPassWord()
+        {
+            return acc.Password;
+        }
+        public string getIDAcc()
+        {
+            return acc.IDAcc;
+        }
     }
 }
